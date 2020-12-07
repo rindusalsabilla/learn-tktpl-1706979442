6fd8a7c297a103a87fac4867c091b7ed1ae6f305
rindu <rindusalsabilla74@yahoo.com>
2020-12-07 23:20:46 +0700

    LAB 6 SEMOGA LANCAR

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/rindu/mylab/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/rindu/mylab/MainActivity.kt
@@ -7,6 +7,8 @@ import android.content.pm.PackageManager
 import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -15,99 +17,25 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var wifiBroadcaster: WifiBroadcaster
-    private lateinit var wifiManager: WifiManager
-    private val MY_PERMISSIONS_ACCESS_COARSE_LOCATION = 1
+    private var num: Int = 100
+    private external fun minNumber(num: Int): Int
+
+    companion object {
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startWifiManager()
-        startView()
-    }
 
-    private fun startWifiManager() {
-        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        if (!wifiManager.isWifiEnabled) {
-            Toast.makeText(this, "Turn On The Wifi", Toast.LENGTH_LONG).show()
-            wifiManager.isWifiEnabled = true
+        btnMin.setOnClickListener {
+            num = minNumber(num)
+            txtNum.text = num.toString()
         }
+
     }
-
-    private fun startView() {
-        scanButton.setOnClickListener {
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                    MY_PERMISSIONS_ACCESS_COARSE_LOCATION
-                )
-            } else {
-                wifiManager.startScan()
-            }
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        unregisterReceiver(wifiBroadcaster)
-    }
-
-    override fun onPostResume() {
-        super.onPostResume()
-        wifiBroadcaster = WifiBroadcaster(wifiManager, listWifi)
-        val intentFilter: IntentFilter = IntentFilter()
-        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
-        registerReceiver(wifiBroadcaster, intentFilter)
-        getWifi()
-    }
-
-    private fun getWifi() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            Toast.makeText(this, "Version Acceptable", Toast.LENGTH_SHORT).show()
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                Toast.makeText(this, "Location turned off", Toast.LENGTH_SHORT).show()
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                    MY_PERMISSIONS_ACCESS_COARSE_LOCATION
-                )
-            } else {
-                Toast.makeText(this, "Location turned on", Toast.LENGTH_SHORT).show()
-                wifiManager.startScan()
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            MY_PERMISSIONS_ACCESS_COARSE_LOCATION -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
-                    wifiManager.startScan()
-                } else {
-                    Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-
-
-
 }
 
 
