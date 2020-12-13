28cf7d50db2d9848d59f9583a6dce082591e96b3
rindu <rindusalsabilla74@yahoo.com>
2020-12-13 17:57:19 +0700

    lab 7

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/rindu/mylab/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/rindu/mylab/MainActivity.kt
@@ -1,40 +1,13 @@
 package id.ac.ui.cs.mobileprogramming.rindu.mylab
 
-import android.Manifest
-import android.content.Context
-import android.content.IntentFilter
-import android.content.pm.PackageManager
-import android.net.wifi.WifiManager
-import android.os.Build
-import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.activity_main.*
-
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    private var num: Int = 100
-    private external fun minNumber(num: Int): Int
-
-    companion object {
-        init {
-            System.loadLibrary("native-lib")
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btnMin.setOnClickListener {
-            num = minNumber(num)
-            txtNum.text = num.toString()
-        }
-
     }
 }
 
