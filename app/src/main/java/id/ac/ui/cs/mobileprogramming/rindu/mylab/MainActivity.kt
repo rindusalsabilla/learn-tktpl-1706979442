8e4b820bb59433222b21e8f2c3e452029c9eb3d3
rindu <rindusalsabilla74@yahoo.com>
2020-12-27 06:29:17 +0700

    string localization

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/rindu/mylab/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/rindu/mylab/MainActivity.kt
@@ -2,11 +2,17 @@ package id.ac.ui.cs.mobileprogramming.rindu.mylab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener{
+            Toast.makeText(this@MainActivity, R.string.btn_clicked, Toast.LENGTH_SHORT).show()
+        }
     }
 }
